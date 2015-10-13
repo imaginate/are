5b48db4bbcc5e5284582b81b3019355ed5b8d5a3
Adam Smith <adamsmith@algorithmiv.com>
2015-10-13 01:05:25 -0700

    Format updates for all is/are parts

--- parts/export.js
+++ parts/export.js
@@ -4,8 +4,10 @@
  * -----------------------------------------------------------------------------
  * @version 0.0.2
  * @see [are]{@link https://github.com/imaginate/are}
+ *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
  * @see [JSDoc3]{@link http://usejsdoc.org/}
  * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
  */
@@ -112,8 +114,6 @@
   // INSERT is-methods.js
 
   // INSERT are-methods.js
-
-  // INSERT node-methods.js
 
   // INSERT main-func-helpers.js
 
--- parts/main-func-helpers.js
+++ parts/main-func-helpers.js
@@ -4,8 +4,10 @@
  * -----------------------------------------------------------------------------
  * @version 0.0.2
  * @see [helpers]{@link https://github.com/imaginate/are/blob/master/parts/main-func-helpers.js}
+ *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
  * @see [JSDoc3]{@link http://usejsdoc.org/}
  * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
  */
