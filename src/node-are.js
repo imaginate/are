90a610e61265d013999af6a2c40a37b379a23574
Adam Smith <adamsmith@algorithmiv.com>
2015-10-13 01:24:13 -0700

    Compiled node-are

--- src/node-are.js
+++ src/node-are.js
@@ -11,89 +11,34 @@
  */
 
 
-;(function(/** Object */ root, /** function(Object) */ setupAre) {
+;(function(/** !{ is: function, are: function } */ _are) {
 
-  /** @type {!{ is: !Object, are: !Object }} */
-  var _are = setupAre();
-  /** @type {!Object} */
-  var checks = {
-    exp: _isObj(typeof exports) && _getObj(exports, true),
-    mod: _isObj(typeof module) && _getObj(module, true),
-    glo: _isObj(typeof global, true) && _getObj(global),
-    win: _isObj(typeof window) && _getObj(window),
-    sel: _isObj(typeof self) && _getObj(self),
-    roo: _isObj(typeof root) && _getObj(root)
-  };
-  checks.glo = checks.exp && checks.mod && checks.glo;
-
-  root = ( checks.glo ?
-    global : checks.win && window !== (root && root.window) ?
-      window : checks.sel ?
-        self : checks.roo ?
-          root : Function('return this')()
-  );
-
-  // window | self | global | this
-  checks.win && _applyAre(window);
-  checks.sel && _applyAre(self);
-  _applyAre(root);
-
-  // exports
-  if (checks.exp && checks.mod) {
-    if (module.exports === exports) {
-      module.exports = _are;
-    }
-    else {
-      _applyAre(exports);
-    }
-  }
-
-  // AMD
-  if (typeof define === 'function' && define.amd &&
-      typeof define.amd === 'object') {
-    define(function() {
-      return _are;
-    });
-  }
+  _applyAre(setupAre);
+  module.exports = setupAre;
 
   /**
-   * @private
-   * @param {string} typeOf
-   * @param {boolean=} noFunc
-   * @return {boolean}
+   * @public
+   * @type {function}
    */
-  function _isObj(typeOf, noFunc) {
-    return typeOf === 'object' || (!noFunc && typeOf === 'function');
-  }
-
-  /**
-   * @private
-   * @param {(Object|?function)} obj
-   * @param {boolean=} testNodeType
-   * @return {boolean}
-   */
-  function _getObj(obj, testNodeType) {
-    obj = obj && testNodeType && obj.nodeType ? false : obj;
-    return obj && !testNodeType && obj.Object !== Object ? false : !!obj;
+  function setupAre() {
+    _applyAre(global);
   }
 
   /**
    * @private
    * @param {(!Object|function)} obj
-   * @return {boolean}
    */
   function _applyAre(obj) {
     obj.is = _are.is;
     obj.Is = _are.Is;
     obj.are = _are.are;
     obj.Are = _are.Are;
-    return true;
   }
 
-})(this, function setupAre(undefined) {
+})((function setupAre(undefined) {
 
   /** @type {boolean} */
-  var _log = typeof console === 'object' && typeof console.log === 'function';
+  var _log = true;
   /** @type {function} */
   var toStr = Object.prototype.toString;
   /** @type {function} */
@@ -104,7 +49,7 @@
   };
   /** @type {function} */
   var logSupportMsg = function(lib, avoid, use) {
-    _log && console.log(
+    console.log(
       'Your JS engine does not support ' + lib + '.' + avoid + '(). ' +
       'Use ' + lib + '.' + use + '() instead.'
     );
@@ -1242,4 +1187,4 @@ function are(typeStr, vals) {
     are: are,
     Are: are
   };
-});
+})());
