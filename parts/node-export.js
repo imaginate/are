873164ab0ba1d0b100a93d3242ffb197b73dac65
Adam Smith <adamsmith@algorithmiv.com>
2015-10-13 01:05:42 -0700

    Created separate export for node-are

---
+++ parts/node-export.js
@@ -0,0 +1,82 @@
+/**
+ * -----------------------------------------------------------------------------
+ * EXPORT NODE-ARE LIBRARIES
+ * -----------------------------------------------------------------------------
+ * @version 0.0.2
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+;(function(/** !{ is: function, are: function } */ _are) {
+
+  _applyAre(setupAre);
+  module.exports = setupAre;
+
+  /**
+   * @public
+   * @type {function}
+   */
+  function setupAre() {
+    _applyAre(global);
+  }
+
+  /**
+   * @private
+   * @param {(!Object|function)} obj
+   */
+  function _applyAre(obj) {
+    obj.is = _are.is;
+    obj.Is = _are.Is;
+    obj.are = _are.are;
+    obj.Are = _are.Are;
+  }
+
+})((function setupAre(undefined) {
+
+  /** @type {boolean} */
+  var _log = true;
+  /** @type {function} */
+  var toStr = Object.prototype.toString;
+  /** @type {function} */
+  var sliceArr = Array.prototype.slice;
+  /** @type {function} */
+  var has = function(obj, prop) {
+    return obj.hasOwnProperty(prop);
+  };
+  /** @type {function} */
+  var logSupportMsg = function(lib, avoid, use) {
+    console.log(
+      'Your JS engine does not support ' + lib + '.' + avoid + '(). ' +
+      'Use ' + lib + '.' + use + '() instead.'
+    );
+  };
+
+  // INSERT is-methods.js
+
+  // INSERT are-methods.js
+
+  // INSERT node-methods.js
+
+  // INSERT main-func-helpers.js
+
+  // INSERT is-main-func.js
+
+  // INSERT are-main-func.js
+
+
+// *****************************************************************************
+// SECTION: END
+// *****************************************************************************
+
+  return {
+    is:  is,
+    Is:  is,
+    are: are,
+    Are: are
+  };
+})());
