28ee1895c15dc1a13a0d388c348565af6d19001a
Adam Smith <adamsmith@algorithmiv.com>
2016-01-06 13:36:44 -0800

    Added tests for new is number checks

--- tests/is-methods.js
+++ tests/is-methods.js
@@ -135,7 +135,21 @@ var methods = {
   others: {
     empty: {
       truthy: [ null, undefined, false, '', 0, {}, [], function(){}, NaN ],
-      falsy: [ true, 'a', 1, { a: null }, [ 'a' ], function(a){} ]
+      falsy:  [ true, 'a', 1, { a: null }, [ 'a' ], function(a){} ]
+    }
+  },
+  number_states: {
+    whole: {
+      truthy: [ -5, 0, 5 ],
+      falsy:  [ -5.5, 1.2 ]
+    },
+    odd: {
+      truthy: [ -1, 1 ],
+      falsy:  [ -2, 2 ]
+    },
+    even: {
+      truthy: [ -2, 0, 2 ],
+      falsy:  [ -1, 1 ]
     }
   }
 };
